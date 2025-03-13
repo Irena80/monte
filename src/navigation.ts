cfdba0f308d7957ff05be53967fa1ff320023017
irena10 <33058713+irena10@users.noreply.github.com>
2025-03-13 12:07:01 +0100

    Push homepage
    Push code

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,117 +3,125 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
+      text: 'Resor',
       links: [
         {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
+          text: 'Företagsresor',
+          href: getPermalink('/resor/foretagsresor'),
         },
         {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
+          text: 'Gruppresor',
+          href: getPermalink('/resor/gruppresor'),
+        },
+      
+        {
+          text: 'Sportresor',
+          href: getPermalink('/resor/sportresor'),
         },
         {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
+          text: 'Herceg Novi',
+          href: getPermalink('/hercegnovi'),
         },
+      
         {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
+          text: 'Budva',
+          href: getPermalink('/budva'),
         },
+       
+        
+        {
+          text: 'Andra destionationer',
+          href: getPermalink('/belgrad'),
+        },
+      
+      
+       
+       
+
       ],
     },
     {
-      text: 'Pages',
+      text: 'Resepaket',
       links: [
+       
         {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
+          text: 'Resepaket',
+          href: getPermalink('/boka'),
         },
         {
-          text: 'Services',
-          href: getPermalink('/services'),
+          text: 'Utflykter',
+          href: getPermalink('/utflykter'),
+        },
+       
+        
+    
+     
+      ],
+    },
+    {
+      text: 'Utforska',
+      links: [
+        
+        {
+          text: 'Saker att göra',
+          href: getPermalink('/utforska'),
         },
         {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
+          text: 'Kusten',
+          href: getPermalink('/kusten'),
+        },
+       
+       
+        {
+          text: 'Inlandet',
+          href: getPermalink('/inlandet'),
         },
         {
-          text: 'About us',
+          text: 'Aktiviteter',
+          href: getPermalink('/aktiviteter'),
+        },
+        {
+          text: 'Hotell',
+          href: getPermalink('/hotell'),
+        },
+        {
+          text: 'Reseinfo',
+          href: getPermalink('/reseinfo'),
+        },
+        
+        {
+          text: 'Blog',
+          href: getBlogPermalink(),
+        },
+      ],
+    },
+  
+    {
+      text: 'Kontakt',
+      links: [
+      
+        {
+          text: 'Kontakt',
+          href: getPermalink('/contact'), 
+        },
+        {
+          text: 'Om oss',
           href: getPermalink('/about'),
         },
         {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
+          text: 'Resevillkor',
           href: getPermalink('/privacy'),
         },
+        {
+          text: 'FQA',
+          href: getPermalink('/terms'),
+        },
+       
+      
+        
       ],
     },
-    {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
-    },
-    {
-      text: 'Widgets',
-      href: '#',
-    },
+  
   ],
   actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
@@ -171,13 +179,13 @@ export const footerData = {
   ],
   socialLinks: [
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/mediteranobay_travel/' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+   
+    
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="../src/assets/images/morecovek.webp" alt="onWidget logo" loading="lazy"></img>
+     <a class="text-blue-600 underline dark:text-muted" href="https://mediteranobay.com/"> Mediteranobay</a> · All rights reserved.
   `,
 };
